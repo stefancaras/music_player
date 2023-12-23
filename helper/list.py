40759084a5b10c3f728a615496835fccef9e53c1
Stefan Caras <stefan.caras@gmail.com>
2023-12-23 13:34:03 +0200

    playlist changes

--- helper/list.py
+++ helper/list.py
@@ -1,3 +1,4 @@
 import os
 with open("../music_list.js", "w") as my_file:
-    my_file.write('export const music_list = ' + str(list(map(lambda e: e[:-4], os.listdir('../music')))))
+    playlist = str(sorted([*map(lambda e: e[:-4], os.listdir('../music'))], key=str.lower))
+    my_file.write('export const music_list = ' + playlist)
